92ce07489596b81d07e63cd03c52cd192820c3b3
Zoran Pesic <zoran1991@gmail.com>
2022-02-04 22:54:00 -0800

    extract keyboard keys to component

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,70 +1,51 @@
 <template>
   <div class="keyboard">
     <div class="keyboard-row">
-      <div class="keyboard-key" @click="handleKeyClick">q</div>
-      <div class="keyboard-key" @click="handleKeyClick">w</div>
-      <div class="keyboard-key" @click="handleKeyClick">e</div>
-      <div class="keyboard-key" @click="handleKeyClick">r</div>
-      <div class="keyboard-key" @click="handleKeyClick">t</div>
-      <div class="keyboard-key" @click="handleKeyClick">y</div>
-      <div class="keyboard-key" @click="handleKeyClick">u</div>
-      <div class="keyboard-key" @click="handleKeyClick">i</div>
-      <div class="keyboard-key" @click="handleKeyClick">o</div>
-      <div class="keyboard-key" @click="handleKeyClick">p</div>
+      <KeyboardKey keyCode="q"/>
+      <KeyboardKey keyCode="w"/>
+      <KeyboardKey keyCode="e"/>
+      <KeyboardKey keyCode="r"/>
+      <KeyboardKey keyCode="t"/>
+      <KeyboardKey keyCode="y"/>
+      <KeyboardKey keyCode="u"/>
+      <KeyboardKey keyCode="i"/>
+      <KeyboardKey keyCode="o"/>
+      <KeyboardKey keyCode="p"/>
     </div>
 
     <div class="keyboard-row">
-      <div class="keyboard-key" @click="handleKeyClick">a</div>
-      <div class="keyboard-key" @click="handleKeyClick">s</div>
-      <div class="keyboard-key" @click="handleKeyClick">d</div>
-      <div class="keyboard-key" @click="handleKeyClick">f</div>
-      <div class="keyboard-key" @click="handleKeyClick">g</div>
-      <div class="keyboard-key" @click="handleKeyClick">h</div>
-      <div class="keyboard-key" @click="handleKeyClick">j</div>
-      <div class="keyboard-key" @click="handleKeyClick">k</div>
-      <div class="keyboard-key" @click="handleKeyClick">l</div>
+      <KeyboardKey keyCode="a"/>
+      <KeyboardKey keyCode="s"/>
+      <KeyboardKey keyCode="d"/>
+      <KeyboardKey keyCode="f"/>
+      <KeyboardKey keyCode="g"/>
+      <KeyboardKey keyCode="h"/>
+      <KeyboardKey keyCode="j"/>
+      <KeyboardKey keyCode="k"/>
+      <KeyboardKey keyCode="l"/>
     </div>
 
     <div class="keyboard-row">
-      <div class="keyboard-key" @click="handleKeyClick">z</div>
-      <div class="keyboard-key" @click="handleKeyClick">x</div>
-      <div class="keyboard-key" @click="handleKeyClick">c</div>
-      <div class="keyboard-key" @click="handleKeyClick">v</div>
-      <div class="keyboard-key" @click="handleKeyClick">b</div>
-      <div class="keyboard-key" @click="handleKeyClick">n</div>
-      <div class="keyboard-key" @click="handleKeyClick">m</div>
-      <div class="keyboard-key" @click="handleKeyClick">i</div>
-      <div class="keyboard-key" @click="handleKeyClick">o</div>
-      <div class="keyboard-key" @click="handleKeyClick">p</div>
-    </div>
-
-    <div class="keyboard-row">
-      <div class="keyboard-key" @click="handleKeyClick">←</div>
-      <div class="keyboard-key" @click="handleKeyClick">⏎</div>
+      <KeyboardKey keyCode="Backspace" keyLabel="⌫"/>
+      <KeyboardKey keyCode="z"/>
+      <KeyboardKey keyCode="x"/>
+      <KeyboardKey keyCode="c"/>
+      <KeyboardKey keyCode="v"/>
+      <KeyboardKey keyCode="b"/>
+      <KeyboardKey keyCode="n"/>
+      <KeyboardKey keyCode="m"/>
+      <KeyboardKey keyCode="Enter" class="keyboard-key-fit-content"/>
     </div>
   </div>
 </template>
 
 <script>
+import KeyboardKey from './KeyboardKey.vue';
+
 export default {
   name: 'Keyboard',
   components: {
-  },
-  props: {
-  },
-  methods: {
-    handleKeyClick(e) {
-      const target = e.target;
-      const keyEvent = new KeyboardEvent(
-        'keydown', {
-          bubbles: true,
-          cancelable: true,
-          key: target.textContent
-        }
-      );
-
-      target.dispatchEvent(keyEvent);
-    }
+    KeyboardKey
   }
 }
 </script>
@@ -72,14 +53,10 @@ export default {
 <style>
 :root {
   --keyboard-background-color: #e7ebf2;
-  --key-background-color: #ffffff;
-  --key-background-color-active: #efefef;
 }
 
 .dark-mode {
   --keyboard-background-color: #585858;
-  --key-background-color: #818181;
-  --key-background-color-active: #707070;
 }
 
 .keyboard {
@@ -93,27 +70,5 @@ export default {
 }
 .keyboard-row:not(:last-child) {
   margin-bottom: 10px;
-}
-.keyboard-key {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 1;
-  height: 40px;
-  width: 28px;
-  border-bottom: 1px solid #b5b5b5;
-  border-radius: 4px;
-  box-shadow: 0 0 3px -1px rgb(0 0 0 / 30%);
-  box-sizing: border-box;
-  background-color: var(--key-background-color);
-  padding: 4px;
-  margin: 0 4px;
-  text-transform: capitalize;
-  font-weight: 400;
-  cursor: pointer;
-  user-select: none;
-}
-.keyboard-key:active {
-  background-color: var(--key-background-color-active);
 }
 </style>
---
+++ src/components/KeyboardKey.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="keyboard-key" @click="handleKeyClick">
+    {{keyLabel}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KeyboardKey',
+  props: {
+    keyCode: {
+      type: String,
+      required: true
+    },
+    keyLabel: {
+      type: String,
+      default: (props) => props.keyCode
+    }
+  },
+  methods: {
+    handleKeyClick() {
+      const keyEvent = new KeyboardEvent(
+        'keydown', {
+          bubbles: true,
+          cancelable: true,
+          key: this.keyCode
+        }
+      );
+
+      this.$el.dispatchEvent(keyEvent);
+    }
+  }
+}
+</script>
+
+<style>
+:root {
+  --key-background-color: #ffffff;
+  --key-background-color-active: #efefef;
+}
+
+.dark-mode {
+  --key-background-color: #818181;
+  --key-background-color-active: #707070;
+}
+
+.keyboard-key {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  height: 40px;
+  width: 28px;
+  border-bottom: 1px solid #b5b5b5;
+  border-radius: 4px;
+  box-shadow: 0 0 3px -1px rgb(0 0 0 / 30%);
+  box-sizing: border-box;
+  background-color: var(--key-background-color);
+  padding: 4px;
+  margin: 0 4px;
+  text-transform: capitalize;
+  font-weight: 400;
+  cursor: pointer;
+  user-select: none;
+}
+.keyboard-key:active {
+  background-color: var(--key-background-color-active);
+}
+.keyboard-key-fit-content {
+  width: fit-content;
+}
+</style>
