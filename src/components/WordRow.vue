3467629defcf04dcb0c90ee4ad65471359e3573e
Zoran Pesic <zoran1991@gmail.com>
2022-02-06 00:26:54 -0800

    remove unused refs

--- src/components/WordRow.vue
+++ src/components/WordRow.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="word-row">
-    <LetterTile ref="word0" :letter="letters[0]" :position="0" :parent="this"/>
-    <LetterTile ref="word1" :letter="letters[1]" :position="1" :parent="this"/>
-    <LetterTile ref="word2" :letter="letters[2]" :position="2" :parent="this"/>
-    <LetterTile ref="word3" :letter="letters[3]" :position="3" :parent="this"/>
-    <LetterTile ref="word4" :letter="letters[4]" :position="4" :parent="this"/>
+    <LetterTile :letter="letters[0]" :position="0" :parent="this"/>
+    <LetterTile :letter="letters[1]" :position="1" :parent="this"/>
+    <LetterTile :letter="letters[2]" :position="2" :parent="this"/>
+    <LetterTile :letter="letters[3]" :position="3" :parent="this"/>
+    <LetterTile :letter="letters[4]" :position="4" :parent="this"/>
   </div>
 </template>
 
