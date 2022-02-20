99c858f982ed72baf94964e071ae3a9495abbb02
Zoran Pesic <zoran1991@gmail.com>
2022-02-20 00:20:47 -0800

    tweak instructions copy

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -4,8 +4,7 @@
 
     <Modal ref="modal">
       <p><strong>How to play</strong></p>
-      <p>Guess the word in 6 tries.</p>
-      <p>Each guess must be a valid 5 letter word. Hit the <em>Enter</em> button to submit.</p>
+      <p>Guess the word in 6 tries. Each guess must be a valid 5 letter word. Hit the <em>Enter</em> button to submit.</p>
       <p>After each guess, the color of the tiles will change to show how close your guess was to the word.</p>
 
       <div class="examples">
